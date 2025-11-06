b4b7b98c279290cd08a62b072444b7d6cb2ba199
amar tech <amartech43@gmail.com>
2025-11-06 19:54:59 +0100

    added properties of username, message, timestamp to chat message. with messagetype

---
+++ ChatMessage.cs
@@ -0,0 +1,20 @@
+﻿namespace Chat_examination
+{
+    public class ChatMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Room { get; set; }
+        public string TargetUser { get; set; }
+        public MessageType Type { get; set; }
+
+    }
+
+    public enum MessageType
+    {
+        Global,
+        Private,
+        Room
+    }
+}
