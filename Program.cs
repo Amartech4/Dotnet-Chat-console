5b9ae97b6b5b37ff2704d6f7b694e0d6fd9bfdab
amar tech <amartech43@gmail.com>
2025-11-07 01:17:09 +0100

    asking user for username

--- Program.cs
+++ Program.cs
@@ -1,9 +1,39 @@
 ﻿namespace Chat_exmination;
 
+
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        // convert int to string input and validate
+
+        string username = GetUsername();
+        Console.WriteLine($"Welcome, {username}! you will join chat room shortly");
+
+        static string GetUsername()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter Username:");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Username cannot be empty.");
+                }
+                else if (int.TryParse(input, out int num))
+                {
+                    return num.ToString();
+                }
+                else
+                {
+                    return input;
+                }
+            }
+        }
+
+
+
     }
 }
